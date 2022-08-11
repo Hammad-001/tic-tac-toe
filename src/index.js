e1c0f4f06fca3f121ad928dd28c445ce73ac277a
Hammad-001 <hammadfaisal178@gmail.com>
2022-08-11 17:54:53 +0500

    updating state using board not square itself

--- src/index.js
+++ src/index.js
@@ -4,13 +4,38 @@ import "./index.css";
 
 class Square extends React.Component {
   render() {
-    return <div className="square">{/* TO DO */}</div>;
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
   render() {
     const status = "Next Player: X";
@@ -23,14 +48,14 @@ class Board extends React.Component {
           {this.renderSquare(2)}
         </div>
         <div className="board-row">
+          {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-          {this.renderSquare(6)}
         </div>
         <div className="board-row">
+          {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-          {this.renderSquare(9)}
         </div>
       </div>
     );
